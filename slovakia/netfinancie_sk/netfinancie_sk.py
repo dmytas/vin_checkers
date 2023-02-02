67ee92c6d6010a953ac2654d7e206b2e866583ca
Yurii <89767190+kiyuni@users.noreply.github.com>
2023-02-02 19:26:36 +0200

    netfinancie_sk

--- slovakia/netfinancie_sk/netfinancie_sk.py
+++ slovakia/netfinancie_sk/netfinancie_sk.py
@@ -1,21 +1,24 @@
 import json
 import requests
-from netfinancie_sk_haeders import headers, body
+from typing import AnyStr, Dict
 from os.path import dirname, abspath
+from netfinancie_sk_haeders import headers, body
 
 cwd_path = dirname(abspath(__file__))
 
 
-def get_vin(path: str):
-    with open(path, "r") as txt_vin:
-        vin_code_list = txt_vin.readlines()
+def get_vins(path: str):
+    vins_file = open(cwd_path, 'r+', encoding='utf-8')
+    vin_code_list = vins_file.readlines()
+    vins_file.close()
+
     for i, line in enumerate(vin_code_list):
         vin_code_list[i] = line.strip()
 
     return vin_code_list
 
 
-def get_car_info(vin):
+def get_car_info(vin: AnyStr):
     url = "https://www.netfinancie.sk/api-xform/"
     new_body = body.copy()
     new_body["search"] = vin
@@ -24,7 +27,7 @@ def get_car_info(vin):
     return json_text
 
 
-def collect_car_info(json_text):
+def collect_car_info(json_text: Dict):
     if not json_text.get('newformdata'):
         return None
     car_info = {
@@ -34,21 +37,23 @@ def collect_car_info(json_text):
     return car_info
 
 
-def get_all_data(path):
-    res = {}
-    vin_code_list = get_vin(path)
+def get_data(vin: AnyStr):
+    json_text = get_car_info(vin)
+    car_info = collect_car_info(json_text)
+    if car_info:
+        json.dump({vin: car_info}, open(f"{cwd_path}/{vin}.json", "w"), indent=4)
+        print(f'{vin} - PARSED!')
+    else:
+        bad_vins = open(f"{cwd_path}/incorrect_vins.txt", "a+")
+        bad_vins.write(vin + "\n")
+        print(f'{vin} - BAD!')
+        bad_vins.close()
+
+    # return res
+
+
+if __name__ == "__main__":
+    vin_code_list = get_vins(f'{cwd_path}/vins.txt')
 
     for vin in vin_code_list:
-        json_text = get_car_info(vin)
-        car_info = collect_car_info(json_text)
-        if car_info:
-            res[vin] = car_info
-            json.dump({vin: car_info}, open(f"{vin}.json", "w"), indent=4)
-        else:
-            with open(f"{cwd_path}/incorrect_vins.txt", "a+") as f:
-                f.write(vin + "\n")
-
-    return res
-
-
-print(get_all_data("vin.txt"))
+        get_data(vin)
