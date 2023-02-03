ecc5059201376bb568ddd87a3999a15a1b14db4c
dmytas <dmytrostepanchukk@gmail.com>
2023-02-03 13:52:41 +0200

    new scraper

--- slovakia/csob_sk/csob_sk.py
+++ slovakia/csob_sk/csob_sk.py
@@ -1,15 +1,57 @@
 import requests
 import json
-from Csob_sk_headers import headers
+from csob_sk_headers import headers
+from os.path import dirname, abspath
+
+cwd_path = dirname(abspath(__file__))
+
+
+def get_vin(path):
+    with open(path, "r") as txt_vin:
+        vin_code_list = txt_vin.readlines()
+        for i, line in enumerate(vin_code_list):
+            vin_code_list[i] = line.strip()
+        return vin_code_list
 
 
 def get_car_info(vin):
     url = f"https://poistenie.csobpoistovna.sk/pzp/api/vehicle/vin/{vin}"
     response = requests.request("GET", url, headers=headers)
-    json_text = json.loads(response.text)
-
+    if response.status_code == 500:
+        return None
+    else:
+        json_text = json.loads(response.text)
     return json_text
 
 
+def collect_car_info(json_text):
+    if json_text is None:
+        return None
+    elif json_text['manufacturer'] is None:
+        return None
+    car_info = {
+        "car_info": json_text
+    }
+
+    return car_info
+
+
+def get_all_data(vin):
+    json_text = get_car_info(vin)
+    car_info = collect_car_info(json_text)
+
+    if car_info:
+        json.dump(car_info, open(f"{cwd_path}/parsed_vins/{vin}.json", "w"), indent=4)
+        print(f'{vin} PARSED!')
+    else:
+        with open(f"{cwd_path}/incorrect_vins.txt", "a+") as f:
+            f.write(vin + "\n")
+        print(f'{vin} BAD VIN!')
+
+
 if __name__ == "__main__":
-    print(get_car_info("WF0UXXGAJU3D64260"))
+    vin_code_list = get_vin(f"{cwd_path}/vin_csob.txt")
+
+    for vin in vin_code_list:
+        get_all_data(vin)
+
